51cb5f1a52a18218b0b9754e53cea8fc55143da4
susahin80 <susahin80@outlook.com.tr>
2023-06-06 18:07:05 +0300

    implement tree insert

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        var tree = new BinarySearchTree();
+        tree.insert(8);
+        tree.insert(3);
+        tree.insert(12);
+        tree.insert(6);
+        tree.insert(7);
+        tree.insert(14);
+        tree.insert(15);
+        tree.insert(16);
+    }
+}
